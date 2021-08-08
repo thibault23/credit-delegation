0f1162cc93e159227bedc14779b11803fe3df8de
thibault23 <thibault.bouzat@essec.edu>
2021-08-08 16:49:47 +0200

    add interfaces

--- contracts/CreditPool.sol
+++ contracts/CreditPool.sol
@@ -5,9 +5,15 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import './IProtocolDataProvider.sol';
-import './IDebtToken.sol';
+import './interfaces/IProtocolDataProvider.sol';
+import './interfaces/IDebtToken.sol';
 
+/*
+    The most simplified version will be transferring one token type to the credit pool,
+    delegating credit in one token type currency to a margin pool,
+    from which one user can tap into and use to invest into one yield farming opportunity.
+    The user will be able to monitor her position.
+*/
 
 contract CreditPool {
 
@@ -16,26 +22,24 @@ contract CreditPool {
 
     mapping(address => uint256) public balances;
     mapping(address => uint256) public delegatedAmounts;
+    uint256 public totalDelegated;
 
     //debt token
-    address constant dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-    address constant marginPool = "";
+    address constant dai = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    //address constant marginPool = "";
     IProtocolDataProvider constant provider = IProtocolDataProvider(address(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d));
 
 
-    function deposit(address aToken, uint256 amount) external {
-        require(IERC20(aToken).balanceOf(msg.sender) != 0, "sender doesn't have tokens");
+    function deposit(address aToken, uint256 amount, address token, address marginPool) external {
+        require(IERC20(aToken).balanceOf(msg.sender) != 0, "sender doesn't have any deposit on Aave");
         balances[msg.sender] += amount;
         IERC20(aToken).safeTransferFrom(msg.sender, address(this), amount);
-        increaseDelegation(token, amount);
+        increaseDelegation(amount, token, marginPool);
     }
 
-    function increaseDelegation(address aToken, uint256 amount) public {
-        delegatedAmounts[msg.sender] += amount;
-        (, , address variableDebtTokenAddress) = provider.getReserveTokensAddresses(dai);
+    function increaseDelegation(uint256 amount, address token, address marginPool) public {
+        totalDelegated += amount;
+        (, , address variableDebtTokenAddress) = provider.getReserveTokensAddresses(token);
         IDebtToken(variableDebtTokenAddress).approveDelegation(marginPool, amount);
-
-    }
-
-    function 
+    } 
 }
